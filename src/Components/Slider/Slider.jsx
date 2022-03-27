16aa75277ae087226eb751de9ced203dfe583f01
jengahmedeyan <jengahmedeyan@gmail.com>
2022-03-27 13:07:07 +0000

    Carousel redesign

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,52 +1,62 @@
 import React from "react";
 import "./Slider.css";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 import bg1 from "../../Assets/bg1.jpg";
 import bg2 from "../../Assets/bg2.jpg";
 import Carousel from "react-bootstrap/Carousel";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
 
 function Slider() {
   return (
-    <div style={{padding: "100px 0px"}}> 
-     <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={bg1}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={bg2}
-      alt="Second slide"
-    />
+    <div style={{ padding: "100px 0px" }} id="main-slider">
+      <Carousel>
+      <Carousel.Item>
+          <img className="d-block w-100" src={bg1} alt="First slide" />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={bg2}
-      alt="Third slide"
-    />
+          <Container>
+            <Row>
+              <Col sm={6}>
+                <Carousel.Caption>
+                  <h2>
+                    We build <span>apps</span> for mobile,
+                    web and desktop.
+                  </h2>
+                  <p>
+                    Want to burst your business with a substantial revenue stream? we'll 
+                    give you the platform to make it happen
+                  </p>
+                  <a href="#features" className="btn btn-primary btn-lg">
+                    Read More
+                  </a>
+                </Carousel.Caption>
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bg2} alt="Second slide" />
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+          <Container>
+            <Row>
+              <Col sm={6}>
+                <Carousel.Caption>
+                  <h2>
+                    Technologies <span>tailored</span> to your specific needs
+                  </h2>
+                  <p>
+                    We adapt our technology to fit each customer's workflow, not
+                    change a customer's workflow to fit our technology.
+                  </p>
+                  <a href="#features" className="btn btn-primary btn-lg">
+                    Read More
+                  </a>
+                </Carousel.Caption>
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
