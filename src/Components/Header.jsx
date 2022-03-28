04c262b7d318b84c1f674bd965de3d54718615a8
jengahmedeyan <jengahmedeyan@gmail.com>
2022-03-28 22:31:57 +0000

    Slider text responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,28 +7,39 @@ import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 const Header = () => {
   return (
-   <div id="home">
-    <Navbar className="justify-content-between fixed-top" 
-    collapseOnSelect expand="md"
-    style={{background:"rgba(255,255,255,.9)",boxShadow:"0 0 3px 0 rgb(0,0,0/10%)"}}
-    > <Container>
-    <Navbar.Brand  href="#home"><img src={logo} alt="Assutech logo" /></Navbar.Brand>
+    <div id="home">
+      <Navbar
+        className="justify-content-between fixed-top"
+        collapseOnSelect
+        expand="md"
+        style={{
+          background: "rgba(255,255,255,.9)",
+          boxShadow: "0 0 3px 0 rgb(0,0,0/10%)",
+        }}
+      >
+        {" "}
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="Assutech logo" />
+          </Navbar.Brand>
 
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-    <Nav className="justify-content-end">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">services</Nav.Link>
-      <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-      <Nav.Link href="#about">About</Nav.Link>
-      <Nav.Link href="#meet-team">Team</Nav.Link>
-      <Nav.Link href="#contact">Contact</Nav.Link>
-    </Nav>
-    </Navbar.Collapse>
-    </Container> 
-  </Navbar>
-  </div>
-
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="justify-content-end">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">services</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#meet-team">Team</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 export default Header;
